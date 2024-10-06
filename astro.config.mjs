064ece9da59a679a39b26fa373513979391cad28
Felix Serrano <exilefe@hotmail.com>
2024-10-06 19:47:34 +0200

    cambio en config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://xilefe.github.io",
-    base: "/",
+    base: "/Astro-test",
     i18n: {
         defaultLocale: "es",
         locales: ["es", "en"],
